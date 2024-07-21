bbcbb723aa508c546c2ba347d7434a4305bb3c1a
Nima Nouri Delouei <nima.nouri.d@gmail.com>
2024-07-21 11:47:34 +0330

    Implement and refine upgrade price and profit calculations for MustEmpire game
    - Add support for various price formulas (fnCompound, fnLinear, fnQuadratic, fnCubic, fnExponential, fnLogarithmic) in calculatePriceNextLevel method.
    - Add support for various profit formulas (fnPayback, fnCompound, fnLinear, fnQuadratic, fnCubic, fnExponential, fnLogarithmic) in calculateProfitNextLevel method.
    - Ensure prices are rounded up to the nearest significant digit using the roundUpPrice method.
    - Validate upgrades against budget and requirements.
    - Rank and filter upgrades based on cost-effectiveness.
    - Log best potential upgrades for future consideration.
    - Handle the purchase of valid upgrades within the budget.

--- app/Pipes/MuskEmpire/HandleUpgrades.php
+++ app/Pipes/MuskEmpire/HandleUpgrades.php
@@ -47,20 +47,21 @@ class HandleUpgrades
 
         foreach ($upgrades as $upgrade) {
             $currentLevel = $this->getCurrentUpgradeLevel($muskEmpireService, $upgrade['key']);
-            $nextLevel = $this->getNextUpgradeLevel($upgrade['levels'], $currentLevel);
+            $nextLevel = $currentLevel + 1;
 
             if ($this->isUpgradeValid($muskEmpireService, $upgrade, $finalBudget, $nextLevel)) {
+                $upgrade['priceNextLevel'] = $this->calculatePriceNextLevel($upgrade, $nextLevel);
+                $upgrade['profitNextLevel'] = $this->calculateProfitNextLevel($upgrade, $nextLevel);
                 $validUpgrades[] = $upgrade;
-                if ($upgrade['priceBasic'] <= $finalBudget) {
+                if ($upgrade['priceNextLevel'] <= $finalBudget) {
                     $potentialUpgrades[] = $upgrade;
                 }
             }
         }
 
         usort($validUpgrades, function ($a, $b) {
-            return ($b['profitBasic'] / $b['priceBasic']) <=> ($a['profitBasic'] / $a['priceBasic']);
+            return ($b['profitNextLevel'] / $b['priceNextLevel']) <=> ($a['profitNextLevel'] / $a['priceNextLevel']);
         });
-
         $this->logBestPotentialUpgrade($validUpgrades, $potentialUpgrades);
 
         return $validUpgrades;
@@ -71,38 +72,30 @@ class HandleUpgrades
         return $muskEmpireService->syncData['skills'][$upgradeKey]['level'] ?? 0;
     }
 
-    private function getNextUpgradeLevel(array $levels, int $currentLevel): array
+    private function isUpgradeValid(MuskEmpireService $muskEmpireService, array $upgrade, float $finalBudget, int $nextLevel): bool
     {
-        foreach ($levels as $level) {
-            if ($level['level'] > $currentLevel) {
-                return $level;
-            }
-        }
-
-        return end($levels);
-    }
-
-    private function isUpgradeValid(MuskEmpireService $muskEmpireService, array $upgrade, float $finalBudget, array $nextLevel): bool
-    {
-        $highestLevel = end($upgrade['levels'])['level'];
-        reset($upgrade['levels']);
+        $highestLevel = $upgrade['maxLevel'];
 
         if ($highestLevel <= $this->getCurrentUpgradeLevel($muskEmpireService, $upgrade['key'])) {
             return false;
         }
-        if (isset($nextLevel['requiredSkills']) && !$this->validateRequiredSkills($muskEmpireService, $nextLevel['requiredSkills'])) {
+
+        $nextLevelRequirements = $this->getLevelRequirements($upgrade['levels'], $nextLevel);
+
+        if ($nextLevelRequirements && !$this->validateRequiredSkills($muskEmpireService, $nextLevelRequirements['requiredSkills'])) {
             return false;
         }
 
-        if ($nextLevel['requiredHeroLevel'] > $muskEmpireService->syncData['hero']['level']) {
+        if ($nextLevelRequirements && $nextLevelRequirements['requiredHeroLevel'] > $muskEmpireService->syncData['hero']['level']) {
             return false;
         }
 
-        if ($nextLevel['requiredFriends'] > $muskEmpireService->syncData['profile']['friends']) {
+        if ($nextLevelRequirements && $nextLevelRequirements['requiredFriends'] > $muskEmpireService->syncData['profile']['friends']) {
             return false;
         }
 
-        if ($upgrade['priceBasic'] > $finalBudget) {
+        $priceNextLevel = $this->calculatePriceNextLevel($upgrade, $nextLevel);
+        if ($priceNextLevel > $finalBudget) {
             return false;
         }
         if (in_array($upgrade['key'], $muskEmpireService->purchasedUpgrades)) {
@@ -112,6 +105,16 @@ class HandleUpgrades
         return true;
     }
 
+    private function getLevelRequirements(array $levels, int $level): ?array
+    {
+        foreach ($levels as $levelData) {
+            if ($levelData['level'] === $level) {
+                return $levelData;
+            }
+        }
+        return null;
+    }
+
     private function validateRequiredSkills(MuskEmpireService $muskEmpireService, array $requiredSkills): bool
     {
         foreach ($requiredSkills as $skill => $level) {
@@ -119,19 +122,96 @@ class HandleUpgrades
                 return false;
             }
         }
-
         return true;
+    }
+
+    private function calculatePriceNextLevel(array $upgrade, int $level): float
+    {
+        $basePrice = $upgrade['priceBasic'];
+        $percentageIncrease = $upgrade['priceFormulaK'];
+
+        switch ($upgrade['priceFormula']) {
+            case 'fnCompound':
+                $increaseFactor = $percentageIncrease / 100;
+                return $this->roundUpPrice($basePrice * pow(1 + $increaseFactor, $level - 1));
+            case 'fnLinear':
+                return $this->roundUpPrice($basePrice + $percentageIncrease * ($level - 1));
+            case 'fnQuadratic':
+                return $this->roundUpPrice($basePrice + $percentageIncrease * pow($level - 1, 2));
+            case 'fnCubic':
+                return $this->roundUpPrice($basePrice + $percentageIncrease * pow($level - 1, 3));
+            case 'fnExponential':
+                return $this->roundUpPrice($basePrice * pow($percentageIncrease, $level - 1));
+            case 'fnLogarithmic':
+                return $this->roundUpPrice($basePrice + $percentageIncrease * log($level));
+            default:
+                return 0;
+        }
+    }
+
+    private function roundUpPrice(float $price): float
+    {
+        if ($price < 100) {
+            return ceil($price / 5) * 5;
+        } elseif ($price < 500) {
+            return ceil($price / 25) * 25;
+        } elseif ($price < 1000) {
+            return ceil($price / 50) * 50;
+        } elseif ($price < 5000) {
+            return ceil($price / 100) * 100;
+        } elseif ($price < 10000) {
+            return ceil($price / 200) * 200;
+        } elseif ($price < 100000) {
+            return ceil($price / 500) * 500;
+        } elseif ($price < 500000) {
+            return ceil($price / 1000) * 1000;
+        } elseif ($price < 1000000) {
+            return ceil($price / 5000) * 5000;
+        } else {
+            return ceil($price / 10000) * 10000;
+        }
+    }
+
+    private function calculateProfitNextLevel(array $upgrade, int $level): float
+    {
+        $profitBasic = $upgrade['profitBasic'];
+        $profitFormulaK = $upgrade['profitFormulaK'];
+
+        switch ($upgrade['profitFormula']) {
+            case 'fnPayback':
+                $totalProfit = 0;
+                for ($i = 1; $i <= $level; $i++) {
+                    $profitIncrement = $profitBasic + $profitFormulaK * ($i - 1);
+                    $totalProfit += $profitIncrement;
+                }
+                return $totalProfit;
+            case 'fnCompound':
+                $increaseFactor = $profitFormulaK / 100;
+                return $profitBasic * pow(1 + $increaseFactor, $level - 1);
+            case 'fnLinear':
+                return $profitBasic + $profitFormulaK * ($level - 1);
+            case 'fnQuadratic':
+                return $profitBasic + $profitFormulaK * pow($level - 1, 2);
+            case 'fnCubic':
+                return $profitBasic + $profitFormulaK * pow($level - 1, 3);
+            case 'fnExponential':
+                return $profitBasic * pow($profitFormulaK, $level - 1);
+            case 'fnLogarithmic':
+                return $profitBasic + $profitFormulaK * log($level);
+            default:
+                return 0;
+        }
     }
 
     private function purchaseUpgrades(MuskEmpireService $muskEmpireService, array $validUpgrades, float &$finalBudget): void
     {
         foreach ($validUpgrades as $upgrade) {
-            if ($finalBudget < $upgrade['priceBasic']) {
+            if ($finalBudget < $upgrade['priceNextLevel']) {
                 continue;
             }
 
             $this->purchaseUpgrade($muskEmpireService, $upgrade['key']);
-            $finalBudget -= $upgrade['priceBasic'];
+            $finalBudget -= $upgrade['priceNextLevel'];
         }
     }
 
@@ -147,14 +227,14 @@ class HandleUpgrades
     {
         if (!empty($potentialUpgrades)) {
             usort($potentialUpgrades, function ($a, $b) {
-                return ($b['profitBasic'] / $b['priceBasic']) <=> ($a['profitBasic'] / $a['priceBasic']);
+                return ($b['profitNextLevel'] / $b['priceNextLevel']) <=> ($a['profitNextLevel'] / $a['priceNextLevel']);
             });
 
             $bestPotentialUpgrade = $potentialUpgrades[0];
             if (!empty($validUpgrades)) {
                 $bestCurrentUpgrade = $validUpgrades[0];
-                $currentValue = $bestCurrentUpgrade['profitBasic'] / $bestCurrentUpgrade['priceBasic'];
-                $potentialValue = $bestPotentialUpgrade['profitBasic'] / $bestPotentialUpgrade['priceBasic'];
+                $currentValue = $bestCurrentUpgrade['profitNextLevel'] / $bestCurrentUpgrade['priceNextLevel'];
+                $potentialValue = $bestPotentialUpgrade['profitNextLevel'] / $bestPotentialUpgrade['priceNextLevel'];
 
                 if ($potentialValue > $currentValue * 2) {
                     Log::info('MuskEmpire | Decided to wait for a better upgrade', ['upgrade' => $bestPotentialUpgrade]);
